51d6291ea565aeccf3f3904b845d1e56bc42366e
Keerthana-Jayakumar <68534351+Keerthana-Jayakumar@users.noreply.github.com>
2020-08-14 23:01:51 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { PlanListComponent } from './plans/plan-list/plan-list.component';
-import { PlanListInputComponent } from './plans/plan-list-input/plan-list-input.component';
+//import { PlanListComponent } from './plans/plan-list/plan-list.component';
+//import { PlanListInputComponent } from './plans/plan-list-input/plan-list-input.component';
 import { NavComponent } from './nav/nav.component';
-import {PlanService} from './plans/Service/plans.service';
-import { ToastrService } from './common/toastr.service';
+//import {PlanService} from './plans/Service/plans.service';
+//import { ToastrService } from './common/toastr.service';
 //import { FormCreationComponent } from './Create-Plan/form-creation/form-creation.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
@@ -14,8 +14,8 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    PlanListComponent,
-    PlanListInputComponent,
+    //PlanListComponent,
+    //PlanListInputComponent,
     NavComponent,
     routingComponents
     //FormCreationComponent,
@@ -25,7 +25,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
     AppRoutingModule
   ],
   exports: [RouterModule],
-  providers: [PlanService, ToastrService],
+  //providers: [PlanService, ToastrService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
